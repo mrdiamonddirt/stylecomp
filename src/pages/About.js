e43c3297da25bcbee32920c520c7ddf873c834cc
RowDog <mrdiamonddirt@gmail.com>
2022-11-01 14:28:07 +0000

    added btn to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,11 @@
+import { PrimaryBtn } from "../components/Buttons";
+
 const About = () => {
     return(
+        <>
         <h1>About</h1>
+        <PrimaryBtn>Button</PrimaryBtn>
+        </>
     )
 }
 export default About
